d58ef2740e8d7eb67378f845e64d6a373d662749
Mark Spanbroek <mark@spanbroek.net>
2018-12-05 09:23:27 +0100

    Implement log likelihood'

--- src/main/java/com/philips/research/regression/LogLikelihoodPrime.java
+++ src/main/java/com/philips/research/regression/LogLikelihoodPrime.java
@@ -6,7 +6,10 @@ import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.lib.collections.Matrix;
 import dk.alexandra.fresco.lib.real.SReal;
 
+import java.util.Arrays;
 import java.util.Vector;
+
+import static java.math.BigDecimal.ZERO;
 
 public class LogLikelihoodPrime implements Computation<Vector<DRes<SReal>>, ProtocolBuilderNumeric> {
     private final DRes<Matrix<DRes<SReal>>> x;
@@ -20,7 +23,25 @@ public class LogLikelihoodPrime implements Computation<Vector<DRes<SReal>>, Prot
     }
 
     @Override
-    public DRes<Vector<DRes<SReal>>> buildComputation(ProtocolBuilderNumeric protocolBuilderNumeric) {
-        return y;
+    public DRes<Vector<DRes<SReal>>> buildComputation(ProtocolBuilderNumeric builder) {
+        DRes<SReal>[] result = GenericArrayUtils.newArray(beta.out().size());
+        Arrays.fill(result, builder.realNumeric().known(ZERO));
+        for (int k=0; k<result.length; k++) {
+            for (int i=0; i<x.out().getRows().size(); i++) {
+                int finalI = i;
+                result[k] =
+                    builder.realNumeric().add(
+                        result[k],
+                        builder.realNumeric().mult(
+                            builder.realNumeric().sub(
+                                y.out().get(i),
+                                builder.seq(new Likelihood(() -> new Vector<>(x.out().getRow(finalI)), beta))
+                            ),
+                            x.out().getRow(i).get(k)
+                        )
+                    );
+            }
+        }
+        return () -> new Vector<>(Arrays.asList(result));
     }
 }
