f9672154a48847b3c8401b367d26a16f567fe249
Mark Spanbroek <mark@spanbroek.net>
2018-12-05 10:52:48 +0100

    Refactor: extract log likelihood' test into separate file

--- src/test/java/com/philips/research/regression/LikelihoodTest.java
+++ src/test/java/com/philips/research/regression/LikelihoodTest.java
@@ -3,7 +3,6 @@ package com.philips.research.regression;
 import dk.alexandra.fresco.framework.Application;
 import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
-import dk.alexandra.fresco.lib.collections.Matrix;
 import dk.alexandra.fresco.lib.real.RealLinearAlgebra;
 import dk.alexandra.fresco.lib.real.SReal;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +12,6 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Vector;
 
-import static com.philips.research.regression.MatrixConstruction.matrix;
 import static java.math.BigDecimal.valueOf;
 import static java.math.RoundingMode.HALF_UP;
 import static java.util.Arrays.asList;
@@ -42,21 +40,6 @@ class LikelihoodTest {
             Vector<BigDecimal> beta = new Vector<>(Collections.singletonList(valueOf(0.1)));
             runner.run(new LikelihoodApplication(xi, beta));
         });
-    }
-
-    @Test
-    @DisplayName("first derivative of log likelihood")
-    void logLikelihoodPrime() {
-        Matrix<BigDecimal> x = matrix(new BigDecimal[][]{
-            {valueOf(1.0), valueOf(2.0), valueOf(3.0), valueOf(4.0)},
-            {valueOf(1.1), valueOf(2.2), valueOf(3.3), valueOf(4.4)}
-        });
-        Vector<BigDecimal> y = new Vector<>(asList(valueOf(0.0), valueOf(1.0)));
-        Vector<BigDecimal> beta = new Vector<>(asList(valueOf(0.1), valueOf(0.2), valueOf(0.3), valueOf(0.4)));
-        Vector<BigDecimal> expected = new Vector<>(asList(valueOf(-0.9134458), valueOf(-1.826892), valueOf(-2.740337), valueOf(-3.653783)));
-        VectorRunner2 runner = new VectorRunner2();
-        Vector<BigDecimal> result = runner.run(x, y, beta, LogLikelihoodPrime::new);
-        VectorAssert.assertEquals(expected, result, 3);
     }
 }
 
