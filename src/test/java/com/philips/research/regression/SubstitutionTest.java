2f9e544599c913bc1b3fc93d0e46e837f9f0a108
Mark Spanbroek <mark@spanbroek.net>
2018-12-05 11:08:12 +0100

    Refactor: replace VectorRunner by a standard runner that runs a MatrixVectorApplication

--- src/test/java/com/philips/research/regression/SubstitutionTest.java
+++ src/test/java/com/philips/research/regression/SubstitutionTest.java
@@ -15,7 +15,7 @@ import static java.util.Arrays.asList;
 @DisplayName("Forward and Backward Substitution")
 class SubstitutionTest {
 
-    private VectorRunner runner = new VectorRunner();
+    private Runner<Vector<BigDecimal>> runner = new Runner<>();
 
     @Test
     @DisplayName("performs forward substitution")
@@ -27,7 +27,7 @@ class SubstitutionTest {
         });
         Vector<BigDecimal> b  = new Vector<>(asList(valueOf(2.0), valueOf(-1.0), valueOf(4.0)));
         Vector<BigDecimal> expected = new Vector<>(asList(valueOf(2.0), valueOf(3.0), valueOf(-16.0)));
-        assertEquals(expected, runner.run(L, b, ForwardSubstitution::new), 3);
+        assertEquals(expected, runner.run(new MatrixVectorApplication(L, b, ForwardSubstitution::new)), 3);
     }
 
     @Test
@@ -40,6 +40,6 @@ class SubstitutionTest {
         });
         Vector<BigDecimal> b  = new Vector<>(asList(valueOf(4.0), valueOf(-1.0), valueOf(2.0)));
         Vector<BigDecimal> expected = new Vector<>(asList(valueOf(-24.0), valueOf(-13.0), valueOf(2.0)));
-        assertEquals(expected, runner.run(L, b, BackSubstitution::new), 3);
+        assertEquals(expected, runner.run(new MatrixVectorApplication(L, b, BackSubstitution::new)), 3);
     }
 }
