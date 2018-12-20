ecf972df1b5c3efa51ae7dddfc3730bc6ba597e9
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-12-20 10:07:08 +0100

    Adds shell script to run logistic regression on the mtcars data set

---
+++ runMtCars.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+test -f target/logistic-regression-jar-with-dependencies.jar || mvn package
+
+echo "Started at $(date)"
+
+main() {
+    out1File=$(mktemp)
+    out2File=$(mktemp)
+    java -jar target/logistic-regression-jar-with-dependencies.jar -i1 -p1:localhost:8871 -p2:localhost:8872 < target/classes/mtcars_party1.txt > ${out1File} &
+    java -jar target/logistic-regression-jar-with-dependencies.jar -i2 -p1:localhost:8871 -p2:localhost:8872 < target/classes/mtcars_party2.txt > ${out2File} &
+    wait
+
+    out1=$(cat ${out1File})
+    out2=$(cat ${out2File})
+
+    if [[ "${out1}" != "${out2}" ]]; then
+        echo "Output is not the same!"
+        echo "Party 1: ${out1}"
+        echo "Party 2: ${out2}"
+    else
+        echo "Output: ${out1}"
+    fi
+}
+
+time main
