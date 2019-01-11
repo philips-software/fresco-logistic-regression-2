73c08cf6eaa04d34d8ceb4c6873fddda9b4d1f77
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-01-11 15:11:35 +0100

    Trying to parallelise logLikelihoodPrime...
    ... doesn't seem to improve performance yet

--- src/main/java/com/philips/research/regression/primitives/LogLikelihoodPrime.java
+++ src/main/java/com/philips/research/regression/primitives/LogLikelihoodPrime.java
@@ -2,7 +2,9 @@ package com.philips.research.regression.primitives;
 
 import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.builder.Computation;
+import dk.alexandra.fresco.framework.builder.ComputationParallel;
 import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.framework.util.Pair;
 import dk.alexandra.fresco.lib.collections.Matrix;
 import dk.alexandra.fresco.lib.real.SReal;
 
@@ -25,24 +27,48 @@ public class LogLikelihoodPrime implements Computation<Vector<DRes<SReal>>, Prot
 
     @Override
     public DRes<Vector<DRes<SReal>>> buildComputation(ProtocolBuilderNumeric builder) {
-        DRes<SReal>[] result = newArray(beta.out().size());
-        Arrays.fill(result, builder.realNumeric().known(ZERO));
-        for (int k=0; k<result.length; k++) {
-            for (int i=0; i<x.out().getRows().size(); i++) {
-                int finalI = i;
-                result[k] =
-                    builder.realNumeric().add(
-                        result[k],
-                        builder.realNumeric().mult(
-                            builder.realNumeric().sub(
-                                y.out().get(i),
-                                builder.seq(new Likelihood(() -> new Vector<>(x.out().getRow(finalI)), beta))
-                            ),
-                            x.out().getRow(i).get(k)
-                        )
-                    );
-            }
+        int betaSize = beta.out().size();
+        return builder.seq(seq -> {
+            DRes<SReal>[] result = newArray(betaSize);
+            Arrays.fill(result, seq.realNumeric().known(ZERO));
+            return () -> new Pair<>(result, 0);
+        }).whileLoop(input -> input.getSecond() < betaSize, (seq, input) -> {
+            DRes<SReal>[] result = input.getFirst();
+            int k = input.getSecond();
+            DRes<SReal> resultK = seq.par(new SingleIteration(k));
+            result[k] = resultK;
+            return () -> new Pair<>(result, k + 1);
+        }).seq((seq, input) -> () -> new Vector<>(asList(input.getFirst())));
+    }
+
+    private class SingleIteration implements ComputationParallel<SReal, ProtocolBuilderNumeric> {
+        private int k;
+        private DRes<SReal> resultK;
+
+        SingleIteration(int k) {
+            this.k = k;
         }
-        return () -> new Vector<>(asList(result));
+
+        @Override
+        public DRes<SReal> buildComputation(ProtocolBuilderNumeric builder) {
+            return builder.seq(seq -> {
+                resultK = seq.realNumeric().known(ZERO);
+                for (int i = 0; i < x.out().getRows().size(); i++) {
+                    int finalI = i;
+                    resultK =
+                        seq.realNumeric().add(
+                            resultK,
+                            seq.realNumeric().mult(
+                                seq.realNumeric().sub(
+                                    y.out().get(i),
+                                    seq.seq(new Likelihood(() -> new Vector<>(x.out().getRow(finalI)), beta))
+                                ),
+                                x.out().getRow(i).get(k)
+                            )
+                        );
+                }
+                return resultK;
+            });
+        }
     }
 }
