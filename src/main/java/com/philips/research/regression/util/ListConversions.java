d9f99020b561c33a677c15432b7f6a7653457268
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-01-15 09:37:38 +0100

    Add helper method to unwrap a vector

--- src/main/java/com/philips/research/regression/util/ListConversions.java
+++ src/main/java/com/philips/research/regression/util/ListConversions.java
@@ -3,6 +3,7 @@ package com.philips.research.regression.util;
 import dk.alexandra.fresco.framework.DRes;
 
 import java.util.List;
+import java.util.Vector;
 import java.util.stream.Collectors;
 
 public class ListConversions {
@@ -12,6 +13,10 @@ public class ListConversions {
 
     public static <T, L extends List<DRes<T>>> List<T> unwrap(DRes<L> list) {
         return list.out().stream().map(DRes::out).collect(Collectors.toList());
+    }
+
+    public static <T, L extends Vector<DRes<T>>> Vector<T> unwrapVector(DRes<L> list) {
+        return list.out().stream().map(DRes::out).collect(Collectors.toCollection(Vector::new));
     }
 
     public interface ElementConversion<T,U> {
