085635c35ef86eab4c56878178c6afc7415c09b4
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-05-31 12:45:03 +0200

    Disables proper random because that doesn't work in FRESCO yet

--- src/main/java/com/philips/research/regression/app/LogisticRegressionApp.java
+++ src/main/java/com/philips/research/regression/app/LogisticRegressionApp.java
@@ -224,7 +224,18 @@ class SpdzRunner <Output> extends ApplicationRunner<Output> {
     private Drbg getDrbg(int myId) {
         // This method was copied from Fresco AbstractSpdzTest
         byte[] seed = new byte[SpdzRunner.PRG_SEED_LENGTH / 8];
-        new Random(myId).nextBytes(seed);
+
+        /* Remark from the FRESCO authors:
+         *
+         * > The joint DRBG needs to be seeded with the same seed for all parties. Seeding it with a 0 byte array there
+         * > instead of one containing the party's ID does the trick.
+         * >
+         * > The above is not secure, but will give you accurate performance results. I've been working on a somewhat
+         * > related issue in a separate branch that will remove the necessity to seed the DRBG manually altogether.
+         * > But at least for the time being, simply removing that line should give you what you need.
+         */
+        // new Random(myId).nextBytes(seed);
+
         return AesCtrDrbgFactory.fromDerivedSeed(seed);
     }
 
