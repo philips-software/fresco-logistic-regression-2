224ce73cd7b3cd01113665e7bd044d6a14cd9367
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-06-11 14:57:41 +0200

    [wip] trying to parallelize things

--- src/main/java/com/philips/research/regression/primitives/Cholesky.java
+++ src/main/java/com/philips/research/regression/primitives/Cholesky.java
@@ -3,6 +3,7 @@ package com.philips.research.regression.primitives;
 import com.philips.research.regression.util.GenericArrayCreation;
 import dk.alexandra.fresco.framework.DRes;
 import dk.alexandra.fresco.framework.builder.Computation;
+import dk.alexandra.fresco.framework.builder.ComputationParallel;
 import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
 import dk.alexandra.fresco.framework.value.SInt;
 import dk.alexandra.fresco.lib.collections.Matrix;
@@ -26,31 +27,66 @@ public class Cholesky implements Computation<Matrix<DRes<SReal>>, ProtocolBuilde
 
     @Override
     public DRes<Matrix<DRes<SReal>>> buildComputation(ProtocolBuilderNumeric builder) {
-        return builder.seq(seq -> {
-            DRes<SReal> one = seq.realNumeric().known(new BigDecimal(1));
-            Matrix<DRes<SReal>> matrix = input.out();
-            int d = matrix.getHeight();
-            DRes<SReal>[][] a = getElements(matrix);
-            for (int j = 0; j < d; j++) {
-                for (int k = 0; k < j; k++) {
-                    // TODO: parallelize
-                    for (int i = j; i < d; i++) {
-                        a[i][j] = seq.realNumeric().sub(
-                            a[i][j],
-                            seq.realNumeric().mult(a[i][k], a[j][k])
-                        );
+        return builder
+            .seq(seq -> {
+                Matrix<DRes<SReal>> matrix = input.out();
+                DRes<SReal>[][] a = getElements(matrix);
+                return () -> new IterationState(a, 0);
+            }).whileLoop(
+                state -> state.j < state.a.length,
+                (seq, state) -> {
+                    DRes<SReal>[][] a = state.a;
+                    int j = state.j;
+                    DRes<SReal> one = seq.realNumeric().known(new BigDecimal(1));
+                    int d = a.length;
+                    for (int k = 0; k < j; k++) {
+                        // TODO: parallelize
+                        for (int i = j; i < d; i++) {
+                            a[i][j] = seq.realNumeric().sub(
+                                a[i][j],
+                                seq.realNumeric().mult(a[i][k], a[j][k])
+                            );
+                        }
                     }
-                }
-                a[j][j] = seq.seq(new RealNumericSqrt(a[j][j]));
-                // TODO: parallelize
-                DRes<SReal> ajj_inverse = seq.realNumeric().div(one, a[j][j]);
-                for (int k = j + 1; k < d; k++) {
-                    a[k][j] = seq.realNumeric().mult(a[k][j], ajj_inverse);
-                }
+                    a[j][j] = seq.seq(new RealNumericSqrt(a[j][j]));
+                    DRes<SReal> ajj_inverse = seq.realNumeric().div(one, a[j][j]);
+                    DRes<DRes<SReal>[][]> newA = seq.par(new AjjInverseThing(a, j, ajj_inverse));
+                    return () -> new IterationState(newA.out(), j + 1);
+            }).seq((seq, state) -> {
+                convertToLowerTriangularMatrix(seq, state.a);
+                return () -> createMatrix(state.a);
+            });
+    }
+
+    private static class IterationState {
+        DRes<SReal>[][] a;
+        int j;
+
+        IterationState(DRes<SReal>[][] a, int j) {
+            this.a = a;
+            this.j = j;
+        }
+    }
+
+    private static class AjjInverseThing implements ComputationParallel<DRes<SReal>[][], ProtocolBuilderNumeric> {
+        private final DRes<SReal>[][] a;
+        private final int j;
+        private final DRes<SReal> ajj_inverse;
+
+        public AjjInverseThing(DRes<SReal>[][] a, int j, DRes<SReal> ajj_inverse) {
+            this.a = a;
+            this.j = j;
+            this.ajj_inverse = ajj_inverse;
+        }
+
+        @Override
+        public DRes<DRes<SReal>[][]> buildComputation(ProtocolBuilderNumeric builder) {
+            for (int k = j + 1; k < a.length; k++) {
+                a[k][j] = builder.realNumeric().mult(a[k][j], ajj_inverse);
             }
-            convertToLowerTriangularMatrix(seq, a);
-            return () -> createMatrix(a);
-        });
+
+            return () -> a;
+        }
     }
 
     private static void convertToLowerTriangularMatrix(ProtocolBuilderNumeric seq, DRes<SReal>[][] a) {
