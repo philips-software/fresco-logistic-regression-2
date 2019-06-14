5e33ac78b1c4b659c1ec672b8d1f62f8f613d6b1
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-06-14 11:02:55 +0200

    Implements DPNoise algorithm from "logreg.pdf"

---
+++ src/main/java/com/philips/research/regression/primitives/DPNoise.java
@@ -0,0 +1,82 @@
+package com.philips.research.regression.primitives;
+
+import dk.alexandra.fresco.framework.DRes;
+import dk.alexandra.fresco.framework.builder.Computation;
+import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.lib.real.SReal;
+
+import java.math.BigDecimal;
+import java.util.Vector;
+
+import static java.math.BigDecimal.valueOf;
+
+/* Implements DPNoise from "logreg.pdf":
+
+   DpNoise[epsilon_, lambda_, len_] := Block[{nv, noiselen, vec},
+       (* calculate random noise: Chaudhuri, Alg 1 *)
+       nv = Length[X[[1]]];
+       (* select length *)
+       noiselen = RandomVariate[GammaDistribution[nv, 2 / (len epsilon lambda)]]; (* create random nv-dimensional sphere *)
+       vec = Table[RandomVariate[NormalDistribution[0, 1]], {i, 1, nv}];
+       Return[vec / Norm[vec] * noiselen]
+   ];
+   DpNoise[epsilon_] := DpNoise[epsilon, 1, Length[X]];
+
+ */
+
+public class DPNoise implements NoiseFactory {
+    private final BigDecimal epsilon;
+    private final BigDecimal lambda;
+    private int length;
+
+    public DPNoise(BigDecimal epsilon, BigDecimal lambda, int length) {
+        this.epsilon = epsilon;
+        this.lambda = lambda;
+        this.length = length;
+    }
+
+    @Override
+    public Computation<Vector<DRes<SReal>>, ProtocolBuilderNumeric> createNoiseGenerator(DRes<Vector<DRes<SReal>>> input) {
+        return new DPNoiseGenerator(input, epsilon, lambda, length);
+    }
+}
+
+class DPNoiseGenerator implements Computation<Vector<DRes<SReal>>, ProtocolBuilderNumeric> {
+    private final DRes<Vector<DRes<SReal>>> input;
+    private final BigDecimal epsilon;
+    private final BigDecimal lambda;
+    private int length;
+
+    public DPNoiseGenerator(DRes<Vector<DRes<SReal>>> input, BigDecimal epsilon, BigDecimal lambda, int length) {
+        this.input = input;
+        this.epsilon = epsilon;
+        this.lambda = lambda;
+        this.length = length;
+    }
+
+    @Override
+    public DRes<Vector<DRes<SReal>>> buildComputation(ProtocolBuilderNumeric builder) {
+        int nv = input.out().size();
+        double shape = nv;
+        double scale = 2.0 / (length * epsilon.doubleValue() * lambda.doubleValue());
+        DRes<SReal> noiseLen = builder.seq(GammaDistribution.random(shape, scale, builder.getRealNumericContext().getPrecision()));
+
+        Vector<DRes<SReal>> noise = new Vector<>();
+        DRes<SReal> sumOfSquares = builder.realNumeric().known(valueOf(0));
+        for (int i = 0; i < nv; ++i) {
+            DRes<SReal> rand = builder.seq(NormalDistribution.random(builder.getRealNumericContext().getPrecision()));
+            noise.add(rand);
+            DRes<SReal> square = builder.realNumeric().mult(rand, rand);
+            sumOfSquares = builder.realNumeric().add(sumOfSquares, square);
+        }
+        DRes<SReal> norm = builder.seq(new RealNumericSqrt(sumOfSquares));
+        DRes<SReal> normInverse = builder.realNumeric().div(builder.realNumeric().known(valueOf(1)), norm);
+        DRes<SReal> normInverseTimesNoiseLen = builder.realNumeric().mult(normInverse, noiseLen);
+        for (int i = 0; i < nv; ++i) {
+            DRes<SReal> scaled = builder.realNumeric().mult(noise.get(i), normInverseTimesNoiseLen);
+            noise.set(i, scaled);
+        }
+
+        return () -> noise;
+    }
+}
---
+++ src/test/java/com/philips/research/regression/primitives/DPNoiseTest.java
@@ -0,0 +1,68 @@
+package com.philips.research.regression.primitives;
+
+import dk.alexandra.fresco.framework.Application;
+import dk.alexandra.fresco.framework.DRes;
+import dk.alexandra.fresco.framework.builder.numeric.ProtocolBuilderNumeric;
+import dk.alexandra.fresco.lib.real.SReal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Vector;
+
+import static com.philips.research.regression.Runner.run;
+import static com.philips.research.regression.util.ListConversions.unwrap;
+import static java.math.BigDecimal.valueOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Meilof DPNoise")
+public class DPNoiseTest {
+    List<BigDecimal> noiseVector;
+    DoubleSummaryStatistics stats;
+
+    @BeforeEach
+    void setUp() {
+        noiseVector = run(new DPNoiseTestApp());
+        stats = noiseVector
+            .stream()
+            .mapToDouble(BigDecimal::doubleValue)
+            .summaryStatistics();
+    }
+
+    @Test
+    @DisplayName("mean is close to zero")
+    public void meanIsCloseToZero() {
+        assertEquals(0.0, stats.getAverage(), 0.1);
+    }
+
+    @Test
+    @DisplayName("it has negative values")
+    public void hasNegativeValues() {
+        assertTrue(stats.getMin() < 0);
+    }
+
+    @Test
+    @DisplayName("it has positive values")
+    public void hasPositiveValues() {
+        assertTrue(stats.getMax() > 0);
+    }
+}
+
+class DPNoiseTestApp implements Application<List<BigDecimal>, ProtocolBuilderNumeric> {
+    @Override
+    public DRes<List<BigDecimal>> buildComputation(ProtocolBuilderNumeric builder) {
+        BigDecimal[] inputs = new BigDecimal[200];
+        for (int i = 0; i < 200; ++i) {
+            inputs[i] = valueOf(0);
+        }
+        DRes<Vector<DRes<SReal>>> closedInputs = builder.realLinAlg().input(new Vector<>(Arrays.asList(inputs)), 1);
+        DRes<Vector<DRes<SReal>>> result = builder.seq(new DPNoiseGenerator(closedInputs, valueOf(1.0 / 5.0), valueOf(1), 100));
+        DRes<Vector<DRes<BigDecimal>>> opened = builder.realLinAlg().openVector(result);
+        return () -> unwrap(opened);
+    }
+}
